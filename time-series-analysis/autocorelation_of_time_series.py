6c69ac0ee3977a5f9bee08e744e8e5984a0741c0
wzagrajcz <wzagrajcz@gmail.com>
2018-02-28 20:25:22 +0100

    Add looking for white noises

--- time-series-analysis/autocorelation_of_time_series.py
+++ time-series-analysis/autocorelation_of_time_series.py
@@ -1,13 +1,14 @@
 from matplotlib import pyplot
 from pandas import read_csv, to_datetime
 from pandas.tools.plotting import autocorrelation_plot
+import os
 
 def plot_autocorelation_of_time_series(df, time_series_name):
     autocorrelation_plot(df[time_series_name].dropna())
     pyplot.title('{} autocorrelation chart'.format(time_series_name))
     pyplot.show()
 
-time_series_csv = 'data/raw-204.csv'
+time_series_csv = os.environ['MSC_DATA'] + '/time_series_csv/raw-204.csv'
 df = read_csv(time_series_csv, header=0, index_col=0)
 df.index = to_datetime(df.index)
 
